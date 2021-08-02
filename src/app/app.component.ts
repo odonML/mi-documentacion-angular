42674e5ded77aa101759484a2fb98d1c4266c53d
Odon_ML <odon@integriapp.com>
2021-08-02 13:50:40 -0500

    directiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  lista = ['hola', 'papu', 'de', 'papus'];
+
+  texto="texto en minusculas";
+  
+  classComponent = 'card';
+
   title = 'mi-documentacion';
-  fecha: number = Date.now();
-  card="card";
-  lista = ["hola", "papu", "de", "papus"];
+  link =
+    'https://images.pexels.com/photos/8647814/pexels-photo-8647814.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+  name = '';
+  addNumber() {
+    console.log('numero agregado');
+  }
 }
